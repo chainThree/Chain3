0074cadb10da56e0b1fdcfd2d1bc2f60adcdabae
Chain3 <bmccurtis@chain3.com>
2018-02-10 13:53:51 -0600

    update
    update

--- truffle.js
+++ truffle.js
@@ -4,9 +4,7 @@ module.exports = {
       host: "localhost",
       port: 7545,
       gas: 6721975,
-      gasPrice: 20000000000,
-      network_id: "5777",
-      from: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57'
+      network_id: "5777"
     }
   },
   solc: {
